2881d17d491d97a0a04f3adbcbd95286ff18d2ec
Caio Goncalves <49763888+potatoprogrammer@users.noreply.github.com>
2020-12-29 19:04:31 -0500

    Add files via upload

---
+++ Day 1/day-1-1-test-your-code/testing_copy.py
@@ -0,0 +1,31 @@
+def test_func():
+    #First *fork* your copy. Then copy-paste your code below this line 👇
+    print("Day 1 - Python Print Function")
+    print("The function is declared like this:")
+    print("what to print")
+    #Finally click "Run" to execute the tests
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ Day 1/day-1-3-exercise/main.py
@@ -0,0 +1,9 @@
+#Write your code below this line 👇
+#in this exercise I ask for an imput from the user, then I print the length of it
+print(len(input("What is your name? ")))
+
+
+
+
+
+
